6f5e9a25b04b092be1f8a6b307a55c34ebb9e656
Cameron2310 <wilke7566@gmail.com>
2022-11-09 16:48:47 -1000

    initial commit

--- car_app/cars_and_brands/models.py
+++ car_app/cars_and_brands/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=50)
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, related_name="cars")
+
+    def __str__(self):
+        return f"{self.brand} {self.name}"
--- car_app/cars_and_brands/views.py
+++ car_app/cars_and_brands/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
-# Create your views here.
+
+# Views for Brands
+def home(request):
+    data = Brand.objects.order_by('name')
+
+    brand = {'brand': data}
+    return render(request, 'home.html', brand)
+
+
+def new(request):
+    if request.method == 'POST':
+        if request.POST.get('name'):
+            brand = Brand()
+            brand.name = request.POST.get('name')
+            brand.save()
+
+    return render(request, 'new.html')
+
+
+def brand_details(request, id):
+    brand = Brand.objects.get(id=id)
+    return render(request, 'brand_details.html', {'brand': brand.__dict__})
+
+
+def edit(request, id):
+    brand = Brand.objects.get(id=id)
+    return render(request, 'edit.html', {'brand': brand.__dict__})
+
+
+def update_record(request, id):
+    name = request.POST.get('name')
+    brand = Brand.objects.get(id=id)
+    brand.name = name
+    brand.save()
+
+    return HttpResponseRedirect(reverse('home'))
+
+
+def cars(request, brand_id):
+    data = Car.objects.filter(brand=brand_id)
+
+    car = {'car': data, 'brand_id': brand_id}
+    return render(request, 'cars.html', car)
+
+
+def new_car(request, brand_id):
+    if request.method == 'POST':
+        if request.POST.get('name'):
+            car = Car()
+            car.name = request.POST.get('name')
+            car.brand = Brand.objects.get(id=brand_id)
+            car.save()
+
+    return render(request, 'new_car.html', {'brand_id': brand_id})
+
+
+def car_details(request, brand_id, car_id):
+    car = Car.objects.get(id=car_id)
+    return render(request, 'car_details.html', {'car': car.__dict__, 'brand_id': brand_id})
+
+
+def edit_car(request, brand_id, car_id):
+    car = Car.objects.get(id=car_id)
+    return render(request, 'edit_car.html', {'car': car.__dict__, 'brand_id': brand_id})
+
+
+def update_car_record(request, brand_id, car_id):
+    name = request.POST.get('name')
+    car = Car.objects.get(id=car_id)
+    car.name = name
+    car.save()
+
+    return HttpResponseRedirect(reverse('home'))
