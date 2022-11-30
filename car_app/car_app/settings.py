ebfd3031c87640bea7e1e5f66e1804d44b2f7b26
Cameron2310 <wilke7566@gmail.com>
2022-11-30 05:14:25 -1000

    hid secret key

--- car_app/car_app/settings.py
+++ car_app/car_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=nk%l-ue(dr&ztis87g%bsbr=10-0zo69fl0w$ip%hh)u#0ug4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
